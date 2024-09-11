8aed66860b161fb08b8d7bdd8c75bb22ed5a74c7
Aditya122221 <116869022+Aditya122221@users.noreply.github.com>
2024-09-11 13:49:14 +0530

    Update Navbar.jsx

--- Frontend/src/JSX/Navbar.jsx
+++ Frontend/src/JSX/Navbar.jsx
@@ -4,21 +4,41 @@ import { Link } from "react-router-dom";
 import Logo from '../Images/logo.png';
 
 export default function Navbar() {
-    return (
-        <div className={N.navbar}>
-            <input type="checkbox" id={N.check} />
-            <label htmlFor="check" className={N.checkbtn}>
-                <span className="material-symbols-outlined" id={N.ham}>menu</span>
-            </label>
-            <Link to='/'><img src={Logo} alt="Logo" className={N.logo} /></Link>
-            <div className={N.menu}>
-                <Link to='/' className={N.s}>Home</Link>
-                <Link to='/about' className={N.s}>About</Link>
-                <Link to='/contact' className={N.s}>Contact</Link>
-                <Link to='/login' className={N.s}>Login</Link>
+    const tk = localStorage.getItem('token')
+    if (tk === null) {
+        return (
+            <div className={N.navbar}>
+                <input type="checkbox" id={N.check} />
+                <label htmlFor="check" className={N.checkbtn}>
+                    <span className="material-symbols-outlined" id={N.ham}>menu</span>
+                </label>
+                <Link to='/'><img src={Logo} alt="Logo" className={N.logo} /></Link>
+                <div className={N.menu}>
+                    <Link to='/' className={N.s}>Home</Link>
+                    <Link to='/about' className={N.s}>About</Link>
+                    <Link to='/contact' className={N.s}>Contact</Link>
+                    <Link to='/login' className={N.s}>Login</Link>
+                </div>
             </div>
-        </div>
-    );
+        );
+    }
+    else {
+        return (
+            <div className={N.navbar}>
+                <input type="checkbox" id={N.check} />
+                <label htmlFor="check" className={N.checkbtn}>
+                    <span className="material-symbols-outlined" id={N.ham}>menu</span>
+                </label>
+                <Link to='/'><img src={Logo} alt="Logo" className={N.logo} /></Link>
+                <div className={N.menu}>
+                    <Link to='/' className={N.s}>Home</Link>
+                    <Link to='/about' className={N.s}>About</Link>
+                    <Link to='/contact' className={N.s}>Contact</Link>
+                    <Link to='/profile' className={N.s}>Profile</Link>
+                </div>
+            </div>
+        )
+    }
 }
 
 /* <div className="search-container">
