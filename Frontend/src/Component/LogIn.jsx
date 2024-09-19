cc46fbf338cc658871622c1cd65066694639fd29
Aditya122221 <116869022+Aditya122221@users.noreply.github.com>
2024-09-19 18:37:46 +0530

    Update LogIn.jsx

--- Frontend/src/Component/LogIn.jsx
+++ Frontend/src/Component/LogIn.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import L from '../CSS/LogIn.module.css';
 import { Link } from "react-router-dom";
 import Logo from '../Images/Logo.png';
@@ -7,67 +7,67 @@ import axios from 'axios';
 import { useNavigate } from "react-router-dom";
 
 const LogIn = () => {
-    const Navigate = useNavigate()
+    const Navigate = useNavigate();
     const [pNumber, setpNumber] = useState('');
     const [password, setPassword] = useState('');
     const [settingUp, setSettingUp] = useState(false);
 
+    useEffect(() => {
+        if (localStorage.getItem('token') !== null) {
+            window.location.href = '/';
+        }
+    })
+
     const handleSubmit = (e) => {
         e.preventDefault();
         const payload = {
-            pNumber: pNumber,
-            password: password
-        }
-        axios.post('http://localhost:3000/login', payload).then((res) => {
-            setSettingUp(false);
-            toast("Log in Successful");
-            console.log("User register", res);
-            localStorage.setItem('token', JSON.stringify(res.data.token))
-            Navigate('/')
-        }).catch((err) => {
-            toast("Log in failed");
-            console.log(err);
-            setSettingUp(false)
-        })
+            pNumber,
+            password
+        };
+
+        setSettingUp(true);
+        axios.post('http://localhost:3000/login', payload)
+            .then((res) => {
+                setSettingUp(false);
+                toast("Log in Successful");
+                console.log("User logged in:", res);
+                localStorage.setItem('token', JSON.stringify(res.data.token));
+                Navigate('/');
+            })
+            .catch((err) => {
+                setSettingUp(false);
+                toast("Log in failed");
+                console.error(err);
+            });
+
         console.log(payload);
-    }
+    };
+
     return (
-        <div className={L.LogInPage}>
-            <header>
-                <Link to="/"><img src={Logo} alt="logo" className={L.logo} /></Link>
-            </header>
-            <div className={L.overall}>
-                <div className={L.socialmedia}>
-                    <div className={L.text} id="socmed">Login using social media account to get quick access</div>
-                    <div className={L.button}>
-                        <button className={L.but} id={L.facebook}>
-                            <a href="#" className={`${L.link} ${L.facebook}`}>Continue with Facebook</a>
-                        </button>
-                        <button className={L.but} id={L.google}>
-                            <a href="#" className={`${L.link} ${L.google}`}>Continue with Google</a>
-                        </button>
-                        <button className={L.but} id="apple">
-                            <a href="#" className={`${L.link} ${L.apple}`}>Continue with Apple</a>
-                        </button>
-                    </div>
+        <div className={L.logger}>
+            <div className={L.container}>
+                <div className={L.socialLogin}>
+                    <header>
+                        <Link to='/'><img className={L.looggo} src={Logo} alt="Logo" /></Link>
+                    </header>
+                    <h3>Login using social media account to get quick access</h3>
+                    <button className={L.facebookButton}>Continue with Facebook</button>
+                    <button className={L.googleButton}>Continue with Google</button>
+                    <button className={L.appleButton}>Continue with Apple</button>
                 </div>
-                <div className={L.log}>
-                    <div className={L.text}>Login to your account</div>
-                    <div className={L.text}>Don't have an account? <Link to='/signup' className={L.sign}>Sign up for free!</Link></div>
-                    <form onSubmit={handleSubmit} className={L.formm}>
-                        <label htmlFor="pNumber" id="pNumber" className={L.required}>Phone Number</label>
-                        <input className={L.inputt} type="text" onChange={(e) => setpNumber(e.target.value)} name="pNumber" placeholder="Enter Phone Number" />
-                        <label htmlFor="password" className={L.required}>Password</label>
-                        <input className={L.inputt} onChange={(e) => setPassword(e.target.value)} type="password" name="password" id="password" placeholder="Enter Password" />
-                        <button type="submit" disabled={settingUp} className={L.submit} id={L.logIn}>
-                            {settingUp ? 'Logging in' : 'Log In'}
-                        </button>
-                    </form>
-                    <button className={L.submit} id={L.forpass}>Forgot Password</button>
+                <div className={L.loginForm}>
+                    <h2>Login to your account</h2>
+                    <p>Don’t have an account? <Link className={L.signer} to="/signup">Sign up for free!</Link></p>
+                    <div className={L.formm}>
+                        <input onChange={(e) => setpNumber(e.target.value)} type="text" placeholder="Enter Phone Number" required className={L.phoneNumberInput} />
+                        <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Enter Password" required className={L.passwordInput} />
+                        <button onClick={handleSubmit} className={L.loginButton} disabled={settingUp}>Log In</button>
+                        <button className={L.forgotPassword}>Forgot Password</button>
+                    </div>
                 </div>
             </div>
         </div>
     );
-}
+};
 
-export default LogIn
+export default LogIn;
