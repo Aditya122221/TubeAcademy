920d0541b3c742cdae0b1391c984cc7d05eee7d7
Aditya122221 <116869022+Aditya122221@users.noreply.github.com>
2024-09-23 21:25:29 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -5,6 +5,12 @@ const userRoutes = require('./Routes/userRoutes')
 
 const app = express()
 
+app.use(cors({
+    origin: ["https://deploy-mern-1whq.vercel.app"],
+    methods: ["POST", "GET"],
+    credentials: true
+}))
+
 app.use(express.json())
 
 app.use(cors({
@@ -14,7 +20,7 @@ app.use(cors({
 
 app.use('/', userRoutes)
 
-mongoose.connect('mongodb://localhost:27017/BestChannel')
+mongoose.connect('mongodb://localhost:27017/TubeAcademy')
     .then(() => console.log('Connected to MongoDB...'))
     .catch((err) => console.log("Failed to connect to Database", err))
 
