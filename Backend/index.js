3ac816335945d8b0128da49f191020c7a417f10e
Aditya122221 <116869022+Aditya122221@users.noreply.github.com>
2024-09-26 19:57:26 +0530

    Update index.js

--- Backend/index.js
+++ Backend/index.js
@@ -4,6 +4,7 @@ const cors = require('cors')
 const userRoutes = require('./Routes/userRoutes')
 
 const app = express()
+const port = process.env.PORT || 3000
 
 app.use(express.json())
 
@@ -18,6 +19,6 @@ mongoose.connect('mongodb://localhost:27017/TubeAcademy')
     .then(() => console.log('Connected to MongoDB...'))
     .catch((err) => console.log("Failed to connect to Database", err))
 
-app.listen(3000, () => {
+app.listen(port, () => {
     console.log("Server running at http://localhost:3000")
 })
