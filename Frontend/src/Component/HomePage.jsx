b48e86a42e4157f66a074262cd87b718fecad0fb
Aditya122221 <116869022+Aditya122221@users.noreply.github.com>
2024-10-07 19:20:39 +0530

    Update HomePage.jsx

--- Frontend/src/Component/HomePage.jsx
+++ Frontend/src/Component/HomePage.jsx
@@ -1,10 +1,9 @@
 import React from 'react';
 import { VideosClassNine, VideosClassTen, VideosClassEleven, VideosClassTwelve } from '../JSX/Classes';
-import { VideosNine } from '../JSX/Function';
+import { VideosNine, Head } from '../JSX/Function';
 import { ClassArray } from '../JSX/Heading';
 import Navbar from '../JSX/Navbar';
 import Footer from '../JSX/Footer';
-import { Head } from '../JSX/Function';
 import HE from '../CSS/Home.module.css';
 import B from '../CSS/Boxex.module.css';
 
@@ -20,20 +19,40 @@ export default function HomePage() {
                     <div className={B.ClassNineVideos}>
                         {VideosClassNine.map(VideosNine)}
                     </div>
+                    <div className={HE.fArrow}>
+                        <span className="material-symbols-outlined">
+                            arrow_forward_ios
+                        </span>
+                    </div>
                 </div>
                 <div className={B.videosClassNine}>
                     <div className={B.ClassNineVideos}>
                         {VideosClassTen.map(VideosNine)}
+                    </div>
+                    <div className={HE.fArrow}>
+                        <span className="material-symbols-outlined">
+                            arrow_forward_ios
+                        </span>
                     </div>
                 </div>
                 <div className={B.videosClassNine}>
                     <div className={B.ClassNineVideos}>
                         {VideosClassEleven.map(VideosNine)}
                     </div>
+                    <div className={HE.fArrow}>
+                        <span className="material-symbols-outlined">
+                            arrow_forward_ios
+                        </span>
+                    </div>
                 </div>
                 <div className={B.videosClassNine}>
                     <div className={B.ClassNineVideos}>
                         {VideosClassTwelve.map(VideosNine)}
+                    </div>
+                    <div className={HE.fArrow}>
+                        <span className="material-symbols-outlined">
+                            arrow_forward_ios
+                        </span>
                     </div>
                 </div>
             </div>
