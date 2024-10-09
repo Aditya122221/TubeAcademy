a73546f09bd2c706060d9839fb1ac5043243a0fa
Aditya122221 <116869022+Aditya122221@users.noreply.github.com>
2024-10-09 10:52:15 +0530

    Update ContactUs.jsx

--- Frontend/src/Component/ContactUs.jsx
+++ Frontend/src/Component/ContactUs.jsx
@@ -1,12 +1,22 @@
-import React, { useRef } from "react";
+import React, { useRef, useState } from "react";
 import Navbar from "../JSX/Navbar";
 import C from '../CSS/ContactUs.module.css'
+import axios from 'axios';
 
 export default function Contact() {
     const aRef = useRef(null);
     const formRef = useRef(null);
     const bRef = useRef(null);
     const reachRef = useRef(null);
+
+    const [succ, setSucc] = useState("");
+    const [unsucc, setUnSucc] = useState("");
+
+    const [contactData, setContactData] = useState({
+        fullname: "Aditya Kumar",
+        email: "adityakumar122221@gmail.com",
+        message: "This is a test message"
+    });
 
     const handleA = () => {
         aRef.current.style.backgroundColor = "#edecec"
@@ -20,19 +30,45 @@ export default function Contact() {
         bRef.current.style.backgroundColor = "#edecec";
         reachRef.current.style.display = "flex";
     }
+
+    const handleChange = (e) => {
+        const { name, value } = e.target
+        setContactData({ ...contactData, [name]: value })
+    }
+
+    const handleSend = async (e) => {
+        e.preventDefault();
+        const alpharegex = /^[a-z A-Z]+$/
+        if (!(alpharegex.test(contactData.name))) {
+            setUnSucc("Name is not valid");
+        } else {
+            let dataSend = {
+                name: contactData.fullname,
+                email: contactData.email,
+                message: contactData.message
+            }
+            console.log(dataSend);
+            axios.post("http://localhost:3000/email", dataSend).then((res) => {
+                setSucc = "Email sent"
+            }).catch((err) => {
+                setUnSucc = "Something went wrong";
+                console.log(err);
+            })
+        }
+    }
     return (
         <>
             <Navbar />
             <div className={C.contact}>
                 <div className={C.button}>
                     <div ref={aRef} onClick={handleA} className={C.contacting}>
-                        <span class="material-symbols-outlined" id={C.elu}>
+                        <span className="material-symbols-outlined" id={C.elu}>
                             connect_without_contact
                         </span>
                         <div className={C.textContact}>Contact Us</div>
                     </div>
                     <div ref={bRef} onClick={handleB} className={C.reachUs}>
-                        <span class="material-symbols-outlined" id={C.elu}>
+                        <span className="material-symbols-outlined" id={C.elu}>
                             rocket_launch
                         </span>
                         <div className={C.textReach}>Reach Us</div>
@@ -45,29 +81,32 @@ export default function Contact() {
                         <div className={C.a3}>
                             <div className={C.a4}>
                                 <label htmlFor="name" className={C.label}>Name</label>
-                                <input type="text" name="name" className={C.input} />
+                                <input value={contactData.fullname} type="text" required name="fullname" className={C.input} onChange={handleChange} />
                             </div>
                             <div className={C.a4}>
                                 <label htmlFor="email" className={C.label}>Email</label>
-                                <input type="email" name="email" className={C.input} />
+                                <input type="email" required value={contactData.email} name="email" className={C.input} onChange={handleChange} />
                             </div>
                         </div>
                         <label htmlFor="message" className={C.label}>Message</label>
-                        <textarea className={C.textArea}></textarea>
+                        <textarea value={contactData.message} onChange={handleChange} required name="message" className={C.textArea}></textarea>
+                        <button onClick={handleSend} className={C.sendButton}>Send</button>
                     </form>
+                    <div className={C.succ}>{succ}</div>
+                    <div className={C.unsucc}>{unsucc}</div>
                 </div>
                 <div ref={reachRef} className={C.reachBox}>
                     <a href="#" className={`${C.b1} ${C.instaa}`}>
-                        <i class="fa-brands fa-instagram"></i>
+                        <i className="fa-brands fa-instagram"></i>
                     </a>
                     <a href="#" className={`${C.b1} ${C.facee}`}>
-                        <i class="fa-brands fa-square-facebook"></i>
+                        <i className="fa-brands fa-square-facebook"></i>
                     </a>
                     <a href="#" className={`${C.b1} ${C.emaa}`}>
-                        <i class="fa-regular fa-envelope"></i>
+                        <i className="fa-regular fa-envelope"></i>
                     </a>
                     <a href="#" className={`${C.b1} ${C.phoo}`} id="pp">
-                        <i class="fa-solid fa-phone"></i>
+                        <i className="fa-solid fa-phone"></i>
                     </a>
                 </div>
             </div>
