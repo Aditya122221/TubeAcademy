7274ad1fbdb3bc6937d837a4ed860d11ccf8a091
Aditya122221 <116869022+Aditya122221@users.noreply.github.com>
2024-10-15 12:42:08 +0530

    Update LogIn.jsx

--- Frontend/src/Component/LogIn.jsx
+++ Frontend/src/Component/LogIn.jsx
@@ -1,20 +1,25 @@
-import React, { useState, useEffect } from "react";
+import React, { useState, useEffect, useRef } from "react";
 import L from '../CSS/LogIn.module.css';
-import { Link } from "react-router-dom";
 import Logo from '../Images/Logo.png';
 import { toast } from "react-toastify";
 import axios from 'axios';
-import { useNavigate } from "react-router-dom";
+import { useNavigate, Link, useLocation } from "react-router-dom";
 
 const LogIn = () => {
     const Navigate = useNavigate();
+    const location = useLocation();
     const [pNumber, setpNumber] = useState('');
     const [password, setPassword] = useState('');
     const [settingUp, setSettingUp] = useState(false);
+    const [Error, setError] = useState("")
+    const [fpnumber, setfpnumber] = useState('')
+
+    const loginRef = useRef()
+    const forgotRef = useRef()
 
     useEffect(() => {
         if (localStorage.getItem('token') !== null) {
-            window.location.href = '/';
+            window.location.href = '/gotLost';
         }
     })
 
@@ -26,7 +31,7 @@ const LogIn = () => {
         };
 
         setSettingUp(true);
-        axios.post('https://tube-academy-backend.onrender.com/login', payload)
+        axios.post('http://localhost:3000/login', payload)
             .then((res) => {
                 setSettingUp(false);
                 toast("Log in Successful");
@@ -36,12 +41,35 @@ const LogIn = () => {
             })
             .catch((err) => {
                 setSettingUp(false);
-                toast("Log in failed");
+                setError("User does not exists")
                 console.error(err);
             });
 
         console.log(payload);
     };
+
+    const handleShowHide = () => {
+        loginRef.current.style.display = "none"
+        forgotRef.current.style.display = "block"
+    }
+
+    const handleL = () => {
+        loginRef.current.style.display = "block"
+        forgotRef.current.style.display = "none"
+    }
+
+    const handleForgot = (e) => {
+        e.preventDefault()
+        const payload = { fpnumber: fpnumber }
+        axios.post('http://localhost:3000/usercheck', payload).then((res) => {
+            console.log(res)
+            Navigate('/forgotpas', { state: fpnumber })
+            setError("")
+        }).catch((e) => {
+            setError("User does not exist")
+            console.log(e)
+        })
+    }
 
     return (
         <div className={L.logger}>
@@ -58,11 +86,17 @@ const LogIn = () => {
                 <div className={L.loginForm}>
                     <h2>Login to your account</h2>
                     <p>Don’t have an account? <Link className={L.signer} to="/signup">Sign up for free!</Link></p>
-                    <div className={L.formm}>
+                    <span className={L.e}>{Error}</span>
+                    <div ref={loginRef} className={L.formm}>
                         <input onChange={(e) => setpNumber(e.target.value)} type="text" placeholder="Enter Phone Number" required className={L.phoneNumberInput} />
                         <input onChange={(e) => setPassword(e.target.value)} type="password" placeholder="Enter Password" required className={L.passwordInput} />
                         <button onClick={handleSubmit} className={L.loginButton} disabled={settingUp}>Log In</button>
-                        <button className={L.forgotPassword}>Forgot Password</button>
+                        <button onClick={handleShowHide} className={L.forgotPassword}>Forgot Password</button>
+                    </div>
+                    <div ref={forgotRef} className={L.forgotPas}>
+                        <input onChange={(e) => setfpnumber(e.target.value)} type="text" placeholder="Enter phone number" required className={L.phoneNumberInput} />
+                        <button onClick={handleForgot} className={L.loginButton}>Next</button>
+                        <button onClick={handleL} className={L.forgotPassword}>Got Remembered</button>
                     </div>
                 </div>
             </div>
