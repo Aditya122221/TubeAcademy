a16c2a5a89390339ca0e2a1d1de1661679da6c8a
Aditya122221 <116869022+Aditya122221@users.noreply.github.com>
2024-12-28 18:50:22 +0530

    Update TeacherProfile.jsx

--- Frontend/src/Component/TeacherProfile.jsx
+++ Frontend/src/Component/TeacherProfile.jsx
@@ -55,14 +55,20 @@ export default function TeacherProfile() {
         <div className={TP.adminProfile}>
             <div className={TP.left}>
                 <div onClick={handleAccount} className={`${TP.left1} ${isAccount ? TP.selectedOne : ''}`}>Account</div>
+
                 <div onClick={handleUpload} className={`${TP.left1} ${isUpload ? TP.selectedOne : ''}`}>Upload Video</div>
+
                 <div onClick={handleLogOut} className={`${TP.left1} ${TP.left2}`}>Log Out</div>
+
                 <Link to='/' className={`${TP.left1} ${TP.left3}`}>Home</Link>
             </div>
             <div className={TP.right}>
                 <div ref={arightRef} className={TP.aright}>
-                    {userData.Registration_ID} {userData.fName} {userData.lName} {userData.email} {userData.address}
-                    <div className={TP.update} onClick={() => Navigate('/updatedata', { state: userData })}>Update Data</div>
+                    <div className={TP.Tthh}>Account Details</div>
+                    <div className={TP.account}>
+                        {userData.Registration_ID} {userData.fName} {userData.lName} {userData.email} {userData.address}
+                        <div className={TP.update} onClick={() => Navigate('/updatedata', { state: userData })}>Update Data</div>
+                    </div>
                 </div>
                 <div ref={brightRef} className={TP.bright}>Upload Video</div>
             </div>
