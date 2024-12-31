fc0004d56daf2cab42c943933ef9c07f290ecc58
Aditya122221 <116869022+Aditya122221@users.noreply.github.com>
2024-12-31 15:49:49 +0530

    mongo atlas conn

--- Backend/index.js
+++ Backend/index.js
@@ -8,30 +8,30 @@ const userRoutes = require('./Routes/userRoutes')
 const app = express()
 const port = process.env.PORT || 3000
 
-app.use(express.json({limit: "16kb"}))
+app.use(express.json({ limit: "16kb" }))
 
 app.use(cors({
-    origin: process.env.CORS_ORIGIN,
-    credentials: true
+  origin: process.env.CORS_ORIGIN,
+  credentials: true
 }))
 
-app.use(express.urlencoded({extended: true, limit: "16kb"}))
+app.use(express.urlencoded({ extended: true, limit: "16kb" }))
 
 app.use('/', userRoutes);
 
 mongoose.connect('mongodb://localhost:27017/TubeAcademy')
-    .then(() => console.log('Connected to MongoDB...'))
-    .catch((err) => console.log("Failed to connect to Database", err))
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch((err) => console.log("Failed to connect to Database", err));
 
 // (async () => {
-//     try {
-//         const c = await mongoose.connect(`${process.env.MONGODB_URI}/TubeAcademy`)
-//         console.log(`Database connected ${c.connection.host}`);
-//     } catch (err) {
-//         console.log(err);
-//     }
-// })
+//   try {
+//     const c = await mongoose.connect("mongodb+srv://adityakumar:aditya123@tubeacademy.zn4s6.mongodb.net/TubeAcademy");
+//     console.log(`Database connected to ${c.connection.host}`);
+//   } catch (err) {
+//     console.error("Error connecting to MongoDB:", err);
+//   }
+// })();
 
 app.listen(port, () => {
-    console.log(`Server running at http://localhost:${port}`)
+  console.log(`Server running at http://localhost:${port}`)
 })
