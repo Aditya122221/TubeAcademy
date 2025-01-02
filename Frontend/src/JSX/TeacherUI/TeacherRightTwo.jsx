ca5ca3956034f757984dd24a0e4aff32193eaab3
Aditya122221 <116869022+Aditya122221@users.noreply.github.com>
2025-01-02 15:59:27 +0530

    some more changes

--- Frontend/src/JSX/TeacherUI/TeacherRightTwo.jsx
+++ Frontend/src/JSX/TeacherUI/TeacherRightTwo.jsx
@@ -7,6 +7,10 @@ export default function TeacherRightTwo() {
     const [thumbnail, setThumbnail] = useState([])
     const [video, setVideo] = useState([])
 
+    const [titleError, setTitleError] = useState("")
+    const [thumbnailError, setThumbnailError] = useState("")
+    const [videoError, setVideoError] = useState("")
+
     const [upload, setUpload] = useState({
         VTitle: "",
         SubjectName: "",
@@ -18,29 +22,29 @@ export default function TeacherRightTwo() {
 
     const handleForm = (e) => {
         e.preventDefault();
-        // const val = validateForm();
-        // if (val) {
+        const val = validateForm();
+        if (val) {
 
-        console.log(thumbnail)
-        console.log(video)
+            console.log(thumbnail)
+            console.log(video)
 
-        const payload = new FormData();
-        payload.append('VTitle', upload.VTitle);
-        payload.append('SubjectName', upload.SubjectName);
-        payload.append('classIn', upload.classIn);
-        payload.append("thumbnail", thumbnail);
-        payload.append("video", video);
+            const payload = new FormData();
+            payload.append('VTitle', upload.VTitle);
+            payload.append('SubjectName', upload.SubjectName);
+            payload.append('classIn', upload.classIn);
+            payload.append("thumbnail", thumbnail);
+            payload.append("video", video);
 
-        axios.post("/api/uploadVideo", payload, {headers:{'Content-Type': 'multipart/form-data'}}).then((res) => {
-            console.log("This part has no error", res.data);
-            succRef.current.style.display = "flex";
-            unsuccRef.current.style.display = "none";
-        }).catch((err) => {
-            unsuccRef.current.style.display = "flex";
-            succRef.current.style.display = "none";
-            console.log(err);
-        })
-        // }
+            axios.post("/api/uploadVideo", payload, { headers: { 'Content-Type': 'multipart/form-data' } }).then((res) => {
+                console.log("This part has no error", res.data);
+                succRef.current.style.display = "flex";
+                unsuccRef.current.style.display = "none";
+            }).catch((err) => {
+                unsuccRef.current.style.display = "flex";
+                succRef.current.style.display = "none";
+                console.log(err);
+            })
+        }
     }
 
     const handleChange = (e) => {
@@ -49,7 +53,29 @@ export default function TeacherRightTwo() {
     }
 
     const validateForm = () => {
-        return true
+        const alpharegex = /^[a-zA-Z0-9 ]+$/
+        const fileTypeOne = ["jpeg", "jpg", "png"]
+        const fileTypeTwo = ["mp4", "mkv", "avi"]
+        if (!(alpharegex.test(upload.VTitle))) {
+            setTitleError("Title should contain only alphabets and numbers")
+            return false
+        }
+
+        if (!fileTypeOne.includes(thumbnail.name.split('.').pop().toLowerCase())) {
+            setThumbnailError("Invalid Image type. Upload only jpeg, jpg or png")
+            return false
+        }
+
+        if (!fileTypeTwo.includes(video.name.split('.').pop().toLowerCase())) {
+            setVideoError("Invalid Video type. Upload only mp4, mkv or avi")
+            return false
+        }
+
+        setTitleError("")
+        setThumbnailError("")
+        setVideoError("")
+        return true;
+
     }
 
     return (
@@ -57,6 +83,8 @@ export default function TeacherRightTwo() {
             <div className={TP.Tthh}>Upload Video</div>
             <div className={TP.account}>
                 <form onSubmit={handleForm} className={TP.form}>
+
+                    <span className={TP.errrooor}>{titleError}</span>
                     <input className={TP.input} type="text" name="VTitle" placeholder="Enter the title for the video" onChange={handleChange} value={upload.VTitle} required />
 
                     <select required onChange={handleChange} value={upload.SubjectName} name="SubjectName" className={TP.select}>
@@ -76,9 +104,11 @@ export default function TeacherRightTwo() {
                     </select>
 
                     <label className={TP.label}>Submit Thumbnail</label>
+                    <span className={TP.errrooor}>{thumbnailError}</span>
                     <input className={TP.file} type="file" required name="thumbnail" onChange={(e) => setThumbnail(e.target.files[0])} />
 
                     <label className={TP.label}>Submit Video</label>
+                    <span className={TP.errrooor}>{videoError}</span>
                     <input className={TP.file} type="file" name="video" onChange={(e) => setVideo(e.target.files[0])} required />
 
                     <button className={TP.button} type="submit">Upload Video</button>
