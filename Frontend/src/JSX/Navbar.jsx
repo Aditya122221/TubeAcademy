ca838fcb10f08010055e8e1385f1d0cfb2e2892f
Aditya122221 <116869022+Aditya122221@users.noreply.github.com>
2025-01-09 20:52:25 +0530

    style: enhance Navbar and card component styles; improve layout, hover effects, and responsiveness

--- Frontend/src/JSX/Navbar.jsx
+++ Frontend/src/JSX/Navbar.jsx
@@ -1,30 +1,50 @@
-import React, { useRef } from "react";
+import React, { useSatae, useRef, useState } from "react";
 import N from '../CSS/Navbar.module.css';
 import { Link } from "react-router-dom";
 import Logo from '../Images/logo.png';
 
 export default function Navbar() {
-    const menuRef = useRef(null)
-    const handleShowMenu = () => {
-        if (menuRef.current.style.display === "none") {
-            menuRef.current.style.display = "flex"
-        }
-        else {
-            menuRef.current.style.display = "none"
-        }
+    const [isActiveH, setIsActiveH] = useState(false);
+    const [isActiveN, setIsActiveN] = useState(false)
+
+
+    function mobileMenu() {
+        setIsActiveH(!setIsActiveH)
+        setIsActiveN(!isActiveN)
+    }
+
+    function closeMenu() {
+        setIsActiveH(false)
+        setIsActiveN(false)
     }
     return (
-        <div className={N.navbar}>
-            <Link to='/home'><img src={Logo} alt="Logo" className={N.logo} /></Link>
-            <span onClick={handleShowMenu} className={`material-symbols-outlined ${N.ham}`}>
-                menu
-            </span>
-            <div ref={menuRef} className={N.menu}>
-                <Link to='/home' className={N.s}>Home</Link>
-                <Link to='/askai' className={N.s}>Ask AI</Link>
-                <Link to='/contact' className={N.s}>Contact</Link>
-                <Link to='/profile' className={N.s}>Profile</Link>
-            </div>
+        <div className={N.navbarr}>
+            <header className={N.header}>
+                <nav className={N.navbar}>
+                    <Link to='/home' className={N.navLogo}>
+                        <img src={Logo} alt="Logo" className={N.logo} />
+                    </Link>
+                    <ul className={`${N.navMenu} ${isActiveN ? N.active : ""}`}>
+                        <li className={N.navItem}>
+                            <Link to="/home" onClick={closeMenu} className={N.navLink}>Home</Link>
+                        </li>
+                        <li className={N.navItem}>
+                            <Link to="/askai" onClick={closeMenu} className={N.navLink}>Ask AI</Link>
+                        </li>
+                        <li className={N.navItem}>
+                            <Link to="/contact" onClick={closeMenu} className={N.navLink}>Contact Us</Link>
+                        </li>
+                        <li className={N.navItem}>
+                            <Link to='/profile' onClick={closeMenu} className={N.navLink}>Profile</Link>
+                        </li>
+                    </ul>
+                    <div onClick={mobileMenu} className={`${N.hamburger} ${isActiveH ? N.active : ""}`}>
+                        <span className={N.bar}></span>
+                        <span className={N.bar}></span>
+                        <span className={N.bar}></span>
+                    </div>
+                </nav>
+            </header>
         </div>
     )
 }
