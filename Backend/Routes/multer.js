034e5231aa68ae64968504a58e634c645de5e4e8
Aditya122221 <adityakumar122221@gmail.com>
2025-06-03 15:38:02 +0530

    feat: add Prettier configuration and ignore files for consistent formatting

--- Backend/Routes/multer.js
+++ Backend/Routes/multer.js
@@ -1,13 +1,14 @@
-import multer from "multer"
+import multer from 'multer'
 
 const storage = multer.diskStorage({
-  destination: function (req, file, cb) {
-    return cb(null, './Images')
-  },
-  filename: function (req, file, cb) {
-    const uniqueSuffix = Date.now() + '-' + Math.round(Math.random() * 1E9)
-    return cb(null, file.originalname + '-' + uniqueSuffix)
-  }
+	destination: function (req, file, cb) {
+		return cb(null, './Images')
+	},
+	filename: function (req, file, cb) {
+		const uniqueSuffix =
+			Date.now() + '-' + Math.round(Math.random() * 1e9)
+		return cb(null, file.originalname + '-' + uniqueSuffix)
+	},
 })
 
 const upload = multer({ storage: storage })
